69f6a69d393207dd43557a23c4bc6ec519d84d8d
shahzebINC <u2017430@giki.edu.pk>
2020-11-01 18:19:42 +0500

    update app

--- devops_assignment/src/App.js
+++ devops_assignment/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shahzeb's app 101
         </p>
         <a
           className="App-link"
